7bad045f5d50f3d6e47412fa9d3b1a247d3b4c22
root <root@Omega-B536.lan>
2020-02-07 01:02:46 +0000

    -Every 8/16h toggle light -Every 1h humidify for 2 min

--- greeting.py
+++ greeting.py
@@ -1,25 +1,35 @@
 # Importing package
 import time
+import os
 import onionGpio
 
-LampPin=1; FoggerPin=45;
-LampObject=onionGpio.OnionGpio(LampPin)
-FoggerObj=onionGpio.OnionGpio(FoggerPin) 
-LampVal=True
+os.system('omega2-ctrl gpiomux set uart1 gpio')
+os.system('omega2-ctrl gpiomux set pwm0 pwm')
 
+LampPin=1; FoggerPin=45; StirPin=15;
+LampObj=onionGpio.OnionGpio(LampPin)
+StirObj=onionGpio.OnionGpio(StirPin)
+FoggerObj=onionGpio.OnionGpio(FoggerPin) 
+LampVal=False
+
+os.system('onion pwm 0 0 1000')
 LampObj.setOutputDirection(LampVal)
 FoggerObj.setOutputDirection(0)
+StirObj.setOutputDirection(1)
 
-while status==0:
-	LampVal=not LampVal
-	status = LampObject.setOutputDirection(LampVal)
+while True:
+	LampObj.setOutputDirection(LampVal)
 	
-	for i in range(0, 11):
+	for i in range(0, 11+(2*LampVal-1)*4):
 		FoggerObj.setOutputDirection(1)
-			time.sleep(12)
-			FoggerObj.setOutputDirection(0)
-			time.sleep(384)
+		os.system('onion pwm 0 85 1000')
+		StirObj.setOutputDirection(1)
+		time.sleep(120)
+		FoggerObj.setOutputDirection(0)
+		os.system('onion pwm 0 0 1000')
+		StirObj.setOutputDirection(0) 
+		time.sleep(3480)
 
+	LampVal=not LampVal
 	print('Lamp Toggled')
-	time.sleep(4)
 
