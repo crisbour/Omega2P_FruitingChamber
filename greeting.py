d760b9c592528246974964e4ac25e066706c5af3
root <root@Omega-B536.lan>
2020-02-08 13:14:11 +0000

    1-Wire

--- greeting.py
+++ greeting.py
@@ -2,6 +2,13 @@
 import time
 import os
 import onionGpio
+from temperatureSensor import TemperatureSensor
+import oneWire
+
+oneWireGpio=11
+def strTemp(sensor):
+	value=sensor.readValue()
+	print("T= "+str(value) +" C")
 
 os.system('omega2-ctrl gpiomux set uart1 gpio')
 os.system('omega2-ctrl gpiomux set pwm0 pwm')
@@ -12,6 +19,14 @@ StirObj=onionGpio.OnionGpio(StirPin)
 FoggerObj=onionGpio.OnionGpio(FoggerPin) 
 LampVal=False
 
+#1-Wire Init
+if not oneWire.setupOneWire(str(oneWireGpio)):
+	print("Kernel mdule could not be inserted. Try again!")
+sensorAddress=oneWire.scanOneAddress()
+sensor=TemperatureSensor("oneWire",{"address":sensorAddress,"gpio":oneWireGpio})
+if not sensor.ready:
+	print("Sensore was not set up correctly.")
+
 os.system('onion pwm 0 0 1000')
 LampObj.setOutputDirection(LampVal)
 FoggerObj.setOutputDirection(0)
@@ -21,10 +36,12 @@ while True:
 	LampObj.setOutputDirection(LampVal)
 	
 	for i in range(0, 11+(2*LampVal-1)*4):
+		strTemp(sensor)
 		FoggerObj.setOutputDirection(1)
 		os.system('onion pwm 0 85 1000')
 		StirObj.setOutputDirection(1)
 		time.sleep(120)
+		strTemp(sensor)
 		FoggerObj.setOutputDirection(0)
 		os.system('onion pwm 0 0 1000')
 		StirObj.setOutputDirection(0) 
@@ -32,4 +49,3 @@ while True:
 
 	LampVal=not LampVal
 	print('Lamp Toggled')
-
